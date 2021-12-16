8926b538dba9fd291c9ac618ac186e0a2e79d2f4
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-12-16 16:43:38 -0300

    se elimina, oculta elementos y se setea propiedades

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ $(document).ready(function() {
     $('.display-4').addClass('text-success');
     //eliminamos la clase
     $('.display-4').removeClass('text-success');
+    $('#content').prepend('<h1>esto es un elemento al inicio</h1>');
+    $('#content').append('<h1>esto es un elemento al final</h1>');
+    //seteo una pripiedad y le doy su valor
+    $('.display-4').css('color', 'blue');
+    //elimina el elemento
+    $('#content').remove();
+    //oculta el elemento
+    $('#content').hide();
 });
 //document.querySelector('.display-4').innerHTML = 'vanilla'
